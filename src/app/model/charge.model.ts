ad6d9266b3581183a6d53921f426d7e7c834f353
JUYI99AYMAN <ayman123juyi@gmail.com>
2021-06-27 22:22:10 +0100

    first commit

---
+++ src/app/model/charge.model.ts
@@ -0,0 +1,10 @@
+export class Charge {
+    idCharge : number;
+    dateCreation : Date ;
+    statusCharge : string;
+    truckNumberCharge : string;
+    originCharge : string;
+    deliveryPointCharge : string;
+    estimatedPointCharge : string;
+    costCharge : string ;
+    }
